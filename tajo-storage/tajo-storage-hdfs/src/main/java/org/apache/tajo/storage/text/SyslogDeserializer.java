9a76d59fd2aee1853428827b01ae0850e0e43753
Jihoon Son <jihoonson@apache.org>
2015-07-03 19:00:00 +0900

    kernlog

--- tajo-storage/tajo-storage-hdfs/src/main/java/org/apache/tajo/storage/text/SyslogDeserializer.java
+++ tajo-storage/tajo-storage-hdfs/src/main/java/org/apache/tajo/storage/text/SyslogDeserializer.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.tajo.storage.text;
 
 import io.netty.buffer.ByteBuf;
@@ -31,10 +49,10 @@ public class SyslogDeserializer extends TextLineDeserializer {
     datums[1] = DatumFactory.createText(tokens[3]);
     datums[2] = DatumFactory.createText(tokens[4]);
     StringBuilder sb = new StringBuilder();
-    for (int i = 6; i < tokens.length; i++) {
+    for (int i = 5; i < tokens.length; i++) {
       sb.append(tokens[i]);
     }
-    datums[4] = DatumFactory.createText(sb.toString());
+    datums[3] = DatumFactory.createText(sb.toString());
     output.put(datums);
   }
 
